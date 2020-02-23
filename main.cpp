fe4e3deb1b2401c1e89b1d8709dd635963fb8a49
lynx <2104934038@qq.com>
2020-02-23 19:50:27 +0800

    temp:找到原始匹配点

--- main.cpp
+++ main.cpp
@@ -36,38 +36,36 @@ int main(int argc, const char * argv[]) {
      * TODO
      */
 
-    /*
     const int CIRCLE_RADIUS    = 3;
     const int CIRCLE_THICKNESS = 1;
     const int LINE_THICKNESS   = 1;
     const int RGB_8U_RANGE     = 256;
 
     // TODO 匹配点配对
-    vector<Point2> f1, f2;
-    RED("%ld", multi_images.images_data.size());
-    Mat result = Mat::zeros(multi_images.images_data[0].img.rows,
-                            multi_images.images_data[0].img.cols,
-                            CV_8UC3);
+    // vector<Point2> f1, f2;
+    // RED("%ld", multi_images.images_data.size());
+    // Mat result = Mat::zeros(multi_images.images_data[0].img.rows,
+    //                         multi_images.images_data[0].img.cols,
+    //                         CV_8UC3);
     multi_images.doFeatureMatching();
     // RED("%ld", multi_images.apap_matching_points.size());// 和图片数相等
-    RED("%ld %ld", multi_images.apap_overlap_mask[0][1].size(),
-                   multi_images.apap_overlap_mask[1][0].size());
-    RED("%ld %ld", multi_images.apap_homographies[0][1].size(),
-                   multi_images.apap_homographies[1][0].size());
-    RED("%ld %ld", multi_images.apap_matching_points[0][1].size(),
-                   multi_images.apap_matching_points[1][0].size());
-    for (int i = 0; i < multi_images.apap_matching_points[0][1].size(); i ++) {
-      f1.emplace_back(multi_images.apap_matching_points[0][1][i]);
-    }
+    // RED("%ld %ld", multi_images.apap_overlap_mask[0][1].size(),
+    //                multi_images.apap_overlap_mask[1][0].size());
+    // RED("%ld %ld", multi_images.apap_homographies[0][1].size(),
+    //                multi_images.apap_homographies[1][0].size());
+    // RED("%ld %ld", multi_images.apap_matching_points[0][1].size(),
+    //                multi_images.apap_matching_points[1][0].size());
+    // for (int i = 0; i < multi_images.apap_matching_points[0][1].size(); i ++) {
+    //   f1.emplace_back(multi_images.apap_matching_points[0][1][i]);
+    // }
 
     // TODO 描绘匹配点
-    multi_images.images_data[0].img.copyTo(result);
-    for (int i = 0; i < f1.size(); i ++) {
-      Scalar color(255, 0, 0);
-      circle(result, f1[i], CIRCLE_RADIUS, color, -1);
-    }
-    imwrite(multi_images.parameter.debug_dir + "fuck.png", result);
-    */
+    // multi_images.images_data[0].img.copyTo(result);
+    // for (int i = 0; i < f1.size(); i ++) {
+    //   Scalar color(255, 0, 0);
+    //   circle(result, f1[i], CIRCLE_RADIUS, color, -1);
+    // }
+    // imwrite(multi_images.parameter.debug_dir + "fuck.png", result);
 
     /**
      *  TODO end
